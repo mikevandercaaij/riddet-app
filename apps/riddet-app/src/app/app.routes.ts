37f9014ede2c6d4944afe871ccd7bc31b7582d3c
Mike van der Caaij <mvdc2000@hotmail.nl>
2022-11-17 12:38:42 +0100

    Fix bootstrap bug

--- apps/riddet-app/src/app/app.routes.ts
+++ apps/riddet-app/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Route } from '@angular/router';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './auth/login/login.component';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+  { path: 'auth/login', pathMatch: 'full', component: LoginComponent },
+  { path: '**', pathMatch: 'full', component: AppComponent },
+];
---
+++ libs/ui/src/lib/ui.module.ts
@@ -0,0 +1,8 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+@NgModule({
+  imports: [CommonModule, NgbModule],
+})
+export class UiModule {}
